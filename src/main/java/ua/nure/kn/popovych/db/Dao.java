d3f8c261b1eb349124284ee4f61872e0535250d2
anastasiiapp <anastasiia.popovych@nure.ua>
2019-12-10 10:05:07 +0200

    lab3

---
+++ src/main/java/ua/nure/kn/popovych/db/Dao.java
@@ -0,0 +1,20 @@
+package ua.nure.kn.popovych.db;
+
+import java.util.Collection;
+
+import ua.nure.kn.popovych.usermanagement.User;
+
+public interface Dao<T> {
+	    T create(T entity) throws DatabaseException;
+
+	    void update(T entity) throws DatabaseException;
+
+	    void delete(T entity) throws DatabaseException;
+
+	    T find(Long id) throws DatabaseException;
+
+	    Collection<T> findAll() throws DatabaseException;
+
+	    void setConnectionFactory(ConnectionFactory connectionFactory);
+
+}
